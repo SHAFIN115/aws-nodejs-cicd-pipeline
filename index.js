c6a111fbd037f475c9e2992fbb348a37d6da48ec
shafin115 <shafinjunayed954@gmail.com>
2026-02-06 22:56:48 +0600

    feat: Adding new initialization of Node js to Code pipeLine

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const os = require("os");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("AWS CodePipeline Node.js App is Live");
+});
+
+const PORT = process.env.PORT || 3000;
+const HOSTNAME = os.hostname();
+
+app.listen(PORT, () => {
+  console.log("Server started successfully");
+  console.log(`Hostname: ${HOSTNAME}`);
+  console.log(`Local URL: http://localhost:${PORT}`);
+});
