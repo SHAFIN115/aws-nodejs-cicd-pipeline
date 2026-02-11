e0c0c4f9dc1857a138c35b20a6f2bf0f93ce6f17
shafin115 <shafinjunayed954@gmail.com>
2026-02-11 22:30:30 +0600

    fix; Added and update request

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const os = require("os");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("AWS CodePipeline Node.js App is Live, Just for testing");
+  res.send("AWS CodePipeline Node.js App is Live. This is now live");
 });
 
 const PORT = process.env.PORT || 3000;
